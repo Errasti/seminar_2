6e8f5881432eb081604fe907c8cc269fcd86e50d
Francisko <errasti007@yandex.ru>
2022-07-12 21:15:32 +0300

    Initial commit

---
+++ proj_203/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+
+if (b % a == 0)
+{
+    Console.WriteLine("Числа кратны!");
+}
+else
+{
+    int c = b % a;
+    Console.WriteLine(c);
+}
