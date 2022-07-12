5c2be752e532b3287c0491ad9aa64db6abf8c57b
Francisko <errasti007@yandex.ru>
2022-07-12 21:03:20 +0300

    Modified proj

--- proj_202/Program.cs
+++ proj_202/Program.cs
@@ -4,3 +4,13 @@ int digitOne = a / 100;
 int digitSecond = a % 10;
 Console.WriteLine(digitOne*10 + digitSecond);
 
+int result = getSecNum(a);
+Console.WriteLine(result);
+int getSecNum(int number)
+{
+    int digitOne = number / 100;
+    int digitSecond = number % 10;
+    int resultDouble = digitOne*10 + digitSecond;
+    return resultDouble;
+}
+
