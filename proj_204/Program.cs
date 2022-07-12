0dab0a22f5bab8aa33667f4033bbf210e9571bdd
Francisko <errasti007@yandex.ru>
2022-07-12 21:39:02 +0300

    Initial Commit

---
+++ proj_204/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите первое число: ");
+
+void divNum ( int number )
+{
+    if (number % 7 == 0 && number % 23 == 0 )
+    {
+        Console.WriteLine("Число " + number + " является кратным 7 и 23");
+    }
+    else
+    {
+        Console.WriteLine("Не кратно!");
+    }
+}
+
+
+int a = int.Parse(Console.ReadLine());
+
+if (a % 7 == 0 && a % 23 == 0 )
+{
+    Console.WriteLine("Число " + a + " является кратным 7 и 23");
+}
+else
+{
+    Console.WriteLine("Не кратно!");
+}
+
