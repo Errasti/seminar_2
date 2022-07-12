c770acca0c520bd4964e5a23b34759128cfa8399
Francisko <errasti007@yandex.ru>
2022-07-12 20:44:35 +0300

    initial commit

---
+++ proj_201/Program.cs
@@ -0,0 +1,28 @@
+﻿int num = new Random().Next(10, 100);
+Console.WriteLine("Random number: " + num);
+
+int firstDigit = num / 10;
+int secondDigit = num % 10;
+
+if (firstDigit > secondDigit) 
+{
+    Console.WriteLine("Max number is " + firstDigit);
+}
+else
+{
+    Console.WriteLine("Max number is " + secondDigit);
+}
+
+Console.WriteLine("Result " + getMax(firstDigit, secondDigit));
+
+int getMax( int a, int b)
+{
+    if ( a > b ) 
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }   
+}
