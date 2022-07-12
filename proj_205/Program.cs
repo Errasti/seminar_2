72de24a426dcbac440b1cf10ea135ec0a98a7b0b
Francisko <errasti007@yandex.ru>
2022-07-12 22:02:52 +0300

    Initial commit

---
+++ proj_205/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите первое число: ");
+int firstNum = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int secondNum = int.Parse(Console.ReadLine());
+
+if (isSqr(firstNum, secondNum))
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+
+
+bool isSqr(int num1, int num2)
+{
+    if (num1 * num1 == num2 || num2 * num2 == num1)
+    {
+        return true ;
+    }
+    else
+    {
+        return false;
+    }
+
+}
+
