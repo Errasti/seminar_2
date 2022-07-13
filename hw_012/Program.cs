cc7af727e8641e607493ba78a8c3fec8f723c593
Francisko <errasti007@yandex.ru>
2022-07-13 22:40:14 +0300

    Initial commit

---
+++ hw_012/Program.cs
@@ -0,0 +1,13 @@
+﻿//Создали метод "вытягивания" второй цифры из трехзначного числа"
+void getDigit( int num )
+{
+    int digit = num / 10;
+    digit = digit % 10;
+    Console.WriteLine("Вторая цифра в " + num + " это " + digit);
+}
+Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+getDigit(number);
+
+
+
