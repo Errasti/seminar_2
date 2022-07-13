c3348a5f79f6cbec0e07352c281541e4a92f5b07
Francisko <errasti007@yandex.ru>
2022-07-13 23:29:01 +0300

    Initial Commit

---
+++ hw_02/Program.cs
@@ -0,0 +1,30 @@
+﻿// Метод поиска третьей цифры в числе
+
+void createNumber( int number )
+{
+    if( number >= 0 & number < 100 )
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+    else
+    {
+        if( number < 0 )
+        {
+            Console.WriteLine("Это отрицательное число!");
+        }
+        else
+        {
+            while( number >= 1000 )
+            {
+                number = number / 10;    
+            }
+            number = number % 10;
+            Console.WriteLine("Третья цифра это - " + number);
+                
+            
+        }
+    }
+}
+Console.WriteLine("Введите число - ");
+int requestedNumber = int.Parse(Console.ReadLine());
+createNumber(requestedNumber);
