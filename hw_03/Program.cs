c29ba9096cce213f52ea55fb76da446c1c5da19a
Francisko <errasti007@yandex.ru>
2022-07-13 23:52:09 +0300

    Initial commit

---
+++ hw_03/Program.cs
@@ -0,0 +1,17 @@
+﻿// Создали программу для выявления выходного дня
+
+Console.Write("Введите число от 1 до 7: ");
+int number = int.Parse(Console.ReadLine());
+if( number >= 1 & number <= 5)
+{
+    Console.WriteLine("Сегодня рабочий день");
+}
+else if (number == 6 || number == 7)
+{
+    Console.WriteLine("Сегодня выходной день");
+}
+else
+{
+    Console.WriteLine("Вы ввели неверное число, перезапустите программу");
+}
+
